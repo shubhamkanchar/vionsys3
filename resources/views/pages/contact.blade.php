d8748a951246173a66072835dbc845f186598209
shubhamkanchar <shubhamkanchar687@gmail.com>
2024-01-08 00:50:36 +0530

    contact updated

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -30,7 +30,7 @@
 					<h2>Find Us</h2>
 				</div>
 			</div>
-			<div class="col-lg-6 col-md-6">
+			<div class="offset-md-3 col-lg-6 col-md-6">
 				<div class="contact-info-content">
 					<h5>Pune Headquarter</h5>
 					<p>{{ config('details.address') }}</p>
@@ -38,14 +38,14 @@
 					<a href="mailto:demo@example.com">{{ config('details.mail') }}</a>
 				</div>
 			</div>
-			<div class="col-lg-6 col-md-6">
+			<!-- <div class="col-lg-6 col-md-6">
 				<div class="contact-info-content">
 					<h5>Latur Office</h5>
 					<p>Office No. W 28 MIDC, Sree Nagar Latur, Maharashtra 413512</p>
 					<a href="tel:0802235678">{{ config('details.phone') }}</a>
 					<a href="mailto:demo@example.com">{{ config('details.mail') }}</a>
 				</div>
-			</div>
+			</div> -->
 			<!-- <div class="col-lg-4 col-md-6">
 					<div class="contact-info-content">
 						<h5>New York Office</h5>
